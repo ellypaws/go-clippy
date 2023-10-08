9138e93bbf0b71e028882d0c55556653d608dce1
Elly <dev@keiau.space>
2023-10-09 07:53:42 +0800

    Excel parser working with local file

--- database/excel.go
+++ database/excel.go
@@ -2,10 +2,9 @@ package bingo
 
 import (
 	"fmt"
-	"net/http"
-	"strings"
-
 	"github.com/PuerkitoBio/goquery"
+	"os"
+	"strings"
 )
 
 //type Function struct {
@@ -18,16 +17,17 @@ import (
 //}
 
 func RecordExcel() []Function {
-	// Make HTTP request
-	response, err := http.Get("https://support.microsoft.com/en-us/office/excel-functions-alphabetical-b3944572-255d-4efb-bb96-c6d90033e188")
-	if err != nil {
-		fmt.Println("Failed to request the webpage")
-		return nil
-	}
-	defer response.Body.Close()
-
-	// Create a goquery document
-	doc, err := goquery.NewDocumentFromReader(response.Body)
+	//response, err := http.Get("https://support.microsoft.com/en-us/office/excel-functions-alphabetical-b3944572-255d-4efb-bb96-c6d90033e188")
+	//if err != nil {
+	//	fmt.Println("Failed to request the webpage")
+	//	return nil
+	//}
+	//defer response.Body.Close()
+	//
+	//doc, err := goquery.NewDocumentFromReader(response.Body)
+	file, _ := os.Open("database/excel.html")
+	defer file.Close()
+	doc, err := goquery.NewDocumentFromReader(file)
 	if err != nil {
 		fmt.Println("Failed to parse the webpage")
 		return nil
@@ -35,21 +35,23 @@ func RecordExcel() []Function {
 
 	var functions []Function
 
-	doc.Find("table.ms-rteTable-0").Each(func(i int, s *goquery.Selection) {
-		s.Find("tbody tr").Each(func(j int, contentSelection *goquery.Selection) {
-			function := Function{}
+	doc.Find("table.banded.flipColors tbody tr").Each(func(i int, tr *goquery.Selection) {
+		funcName := tr.Find("td:first-child p a")
+		categoryDesc := strings.Split(tr.Find("td:nth-child(2) p").Text(), ":")
+		url := funcName.AttrOr("href", "")
+		// append the base url if the url is relative
+		if !strings.HasPrefix(url, "http") {
+			url = "https://support.microsoft.com" + url
+		}
 
-			contentSelection.Find("td").Each(func(k int, td *goquery.Selection) {
-				if k == 0 {
-					function.Name = strings.Split(td.Text(), " ")[0]
-					function.URL, _ = td.Find("a").Attr("href")
-				} else if k == 1 {
-					function.Category = strings.Split(td.Text(), ":")[0]
-				}
-			})
+		function := Function{
+			Name:        strings.TrimSpace(strings.Split(funcName.Text(), " ")[0]),
+			URL:         url,
+			Category:    strings.TrimSpace(categoryDesc[0]),
+			Description: strings.TrimSpace(categoryDesc[1]),
+		}
 
-			functions = append(functions, function)
-		})
+		functions = append(functions, function)
 	})
 
 	return functions
--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module go-clippy
 go 1.21
 
 require (
-	github.com/bwmarrin/discordgo v0.27.1 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/bwmarrin/discordgo v0.27.1
+	github.com/nokusukun/bingo v0.1.6
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -13,7 +19,6 @@ require (
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/nokusukun/bingo v0.1.6 // indirect
 	go.etcd.io/bbolt v1.3.7 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.16.0 // indirect
