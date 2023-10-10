abcca45b6f3aa0c15b1bd4d92bffe26167c4cfd1
Elly <dev@keiau.space>
2023-10-10 13:49:58 +0800

    Refactor commands to variables to easily add more reusable options.
    Two new variables are added for the reusable ApplicationCommandOption in commands.go:15,54
    
    Also refactored commands as a `map[string]*discordgo.ApplicationCommand` just to have a descriptive name associated with each ApplicationCommand.
    
    The string key is not used unless ApplicationCommand.Name is a blank string. (avoid this)

--- bot/commands.go
+++ bot/commands.go
@@ -2,90 +2,130 @@ package discord
 
 import "github.com/bwmarrin/discordgo"
 
-var commands = []*discordgo.ApplicationCommand{
-	{
+const (
+	requiredFunction = iota
+	optionalFunctionAutocomplete
+	optionalDescriptionAutocomplete
+	optionalPlatformSelection
+
+	maskedUser
+	maskedChannel
+)
+
+var commandOptions = map[int]*discordgo.ApplicationCommandOption{
+	requiredFunction: {
+		Type:        discordgo.ApplicationCommandOptionString,
+		Name:        "function",
+		Description: "The function to look up",
+		Required:    true,
+	},
+	optionalFunctionAutocomplete: {
+		Type:         discordgo.ApplicationCommandOptionString,
+		Name:         "function",
+		Description:  "The function to look up",
+		Required:     false,
+		Autocomplete: true,
+	},
+	optionalDescriptionAutocomplete: {
+		Type:         discordgo.ApplicationCommandOptionString,
+		Name:         "description",
+		Description:  "The description to look up",
+		Required:     false,
+		Autocomplete: true,
+	},
+	optionalPlatformSelection: {
+		Type:        discordgo.ApplicationCommandOptionString,
+		Name:        "platform",
+		Description: "Excel or Google Sheets",
+		Required:    false,
+		Choices: []*discordgo.ApplicationCommandOptionChoice{
+			{
+				Name:  "Excel",
+				Value: "excel",
+			},
+			{
+				Name:  "Google Sheets",
+				Value: "sheets",
+			},
+		},
+	},
+}
+
+var maskedOptions = map[int]*discordgo.ApplicationCommandOption{
+	maskedUser: {
+		Type:        discordgo.ApplicationCommandOptionUser,
+		Name:        "user-option",
+		Description: "User option",
+		Required:    false,
+	},
+	maskedChannel: {
+		Type:        discordgo.ApplicationCommandOptionChannel,
+		Name:        "channel-option",
+		Description: "Channel option",
+		// Channel type mask
+		ChannelTypes: []discordgo.ChannelType{
+			discordgo.ChannelTypeGuildText,
+			discordgo.ChannelTypeGuildVoice,
+		},
+		Required: false,
+	},
+}
+
+var commands = map[string]*discordgo.ApplicationCommand{
+	"Saying hello": {
 		Name: "hello",
 		// All commands and options must have a description
 		// Commands/options without description will fail the registration
 		// of the command.
 		Description: "Say hello to the bot",
 	},
-	{
+	"permission-overview": {
 		Name:                     "permission-overview",
 		Description:              "Command for demonstration of default command permissions",
 		DefaultMemberPermissions: &defaultMemberPermissions,
 		DMPermission:             &dmPermission,
 	},
-	//{
-	//	Name:        "basic-command-with-files",
-	//	Description: "Basic command with files",
-	//},
-	//{
-	//	Name:        "localized-command",
-	//	Description: "Localized command. Description and name may vary depending on the Language setting",
-	//	NameLocalizations: &map[discordgo.Locale]string{
-	//		discordgo.ChineseCN: "本地化的命令",
-	//	},
-	//	DescriptionLocalizations: &map[discordgo.Locale]string{
-	//		discordgo.ChineseCN: "这是一个本地化的命令",
-	//	},
-	//	Options: []*discordgo.ApplicationCommandOption{
-	//		{
-	//			Name:        "localized-option",
-	//			Description: "Localized option. Description and name may vary depending on the Language setting",
-	//			NameLocalizations: map[discordgo.Locale]string{
-	//				discordgo.ChineseCN: "一个本地化的选项",
-	//			},
-	//			DescriptionLocalizations: map[discordgo.Locale]string{
-	//				discordgo.ChineseCN: "这是一个本地化的选项",
-	//			},
-	//			Type: discordgo.ApplicationCommandOptionInteger,
-	//			Choices: []*discordgo.ApplicationCommandOptionChoice{
-	//				{
-	//					Name: "First",
-	//					NameLocalizations: map[discordgo.Locale]string{
-	//						discordgo.ChineseCN: "一的",
-	//					},
-	//					Value: 1,
-	//				},
-	//				{
-	//					Name: "Second",
-	//					NameLocalizations: map[discordgo.Locale]string{
-	//						discordgo.ChineseCN: "二的",
-	//					},
-	//					Value: 2,
-	//				},
-	//			},
-	//		},
-	//	},
-	//},
-	{
+	"Basic response testing": {
+		Name:        "responses",
+		Description: "Interaction responses testing initiative",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name:        "resp-type",
+				Description: "Response type",
+				Type:        discordgo.ApplicationCommandOptionInteger,
+				Choices: []*discordgo.ApplicationCommandOptionChoice{
+					{
+						Name:  "Channel message with source",
+						Value: 4,
+					},
+					{
+						Name:  "Deferred response With Source",
+						Value: 5,
+					},
+				},
+				Required: true,
+			},
+		},
+	},
+	"Followup testing": {
+		Name:        "followups",
+		Description: "Followup messages",
+	},
+	"/solved": {
+		Name:        "solved",
+		Description: "Mark a question as solved and optionally close the thread",
+		Options: []*discordgo.ApplicationCommandOption{
+			maskedOptions[maskedUser],
+			maskedOptions[maskedChannel],
+		},
+	},
+	"/function": {
 		Name:        "function",
 		Description: "Look up a function in Excel or Google Sheets",
 		Options: []*discordgo.ApplicationCommandOption{
+			commandOptions[requiredFunction],
+			commandOptions[optionalPlatformSelection],
 
-			{
-				Type:        discordgo.ApplicationCommandOptionString,
-				Name:        "function",
-				Description: "The function to look up",
-				Required:    true,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionString,
-				Name:        "platform",
-				Description: "Excel or Google Sheets",
-				Required:    false,
-				Choices: []*discordgo.ApplicationCommandOptionChoice{
-					{
-						Name:  "Excel",
-						Value: "excel",
-					},
-					{
-						Name:  "Google Sheets",
-						Value: "sheets",
-					},
-				},
-			},
 			//{
 			//	Type:        discordgo.ApplicationCommandOptionInteger,
 			//	Name:        "integer-option",
@@ -137,90 +177,103 @@ var commands = []*discordgo.ApplicationCommand{
 			//},
 		},
 	},
-	//{
-	//	Name:        "subcommands",
-	//	Description: "Subcommands and command groups example",
-	//	Options: []*discordgo.ApplicationCommandOption{
-	//		// When a command has subcommands/subcommand groups
-	//		// It must not have top-level options, they aren't accesible in the UI
-	//		// in this case (at least not yet), so if a command has
-	//		// subcommands/subcommand any groups registering top-level options
-	//		// will cause the registration of the command to fail
-	//
-	//		{
-	//			Name:        "subcommand-group",
-	//			Description: "Subcommands group",
-	//			Options: []*discordgo.ApplicationCommandOption{
-	//				// Also, subcommand groups aren't capable of
-	//				// containing options, by the name of them, you can see
-	//				// they can only contain subcommands
-	//				{
-	//					Name:        "nested-subcommand",
-	//					Description: "Nested subcommand",
-	//					Type:        discordgo.ApplicationCommandOptionSubCommand,
-	//				},
-	//			},
-	//			Type: discordgo.ApplicationCommandOptionSubCommandGroup,
-	//		},
-	//		// Also, you can create both subcommand groups and subcommands
-	//		// in the command at the same time. But, there's some limits to
-	//		// nesting, count of subcommands (top level and nested) and options.
-	//		// Read the intro of slash-commands docs on Discord dev portal
-	//		// to get more information
-	//		{
-	//			Name:        "subcommand",
-	//			Description: "Top-level subcommand",
-	//			Type:        discordgo.ApplicationCommandOptionSubCommand,
-	//		},
-	//	},
-	//},
-	{
-		Name:        "responses",
-		Description: "Interaction responses testing initiative",
+	"/search": {
+		Name:        "search",
+		Description: "Showcase of multiple autocomplete option",
+		Type:        discordgo.ChatApplicationCommand,
 		Options: []*discordgo.ApplicationCommandOption{
-			{
-				Name:        "resp-type",
-				Description: "Response type",
-				Type:        discordgo.ApplicationCommandOptionInteger,
-				Choices: []*discordgo.ApplicationCommandOptionChoice{
-					{
-						Name:  "Channel message with source",
-						Value: 4,
-					},
-					{
-						Name:  "Deferred response With Source",
-						Value: 5,
-					},
-				},
-				Required: true,
-			},
-		},
-	},
-	{
-		Name:        "followups",
-		Description: "Followup messages",
-	},
-	{
-		Name:        "solved",
-		Description: "Mark a question as solved and optionally close the thread",
-		Options: []*discordgo.ApplicationCommandOption{
-			{
-				Type:        discordgo.ApplicationCommandOptionUser,
-				Name:        "user-option",
-				Description: "User option",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionChannel,
-				Name:        "channel-option",
-				Description: "Channel option",
-				// Channel type mask
-				ChannelTypes: []discordgo.ChannelType{
-					discordgo.ChannelTypeGuildText,
-					discordgo.ChannelTypeGuildVoice,
-				},
-				Required: false,
-			},
+			commandOptions[optionalFunctionAutocomplete],
+			commandOptions[optionalDescriptionAutocomplete],
+			commandOptions[optionalPlatformSelection],
 		},
 	},
 }
+
+// ----- UNUSED COMMANDS -----
+
+//var unusedCommands = map[string]*discordgo.ApplicationCommand{
+
+//"basic-command-with-files": {
+//	Name:        "basic-command-with-files",
+//	Description: "Basic command with files",
+//},
+
+//"localization": {
+//	Name:        "localized-command",
+//	Description: "Localized command. Description and name may vary depending on the Language setting",
+//	NameLocalizations: &map[discordgo.Locale]string{
+//		discordgo.ChineseCN: "本地化的命令",
+//	},
+//	DescriptionLocalizations: &map[discordgo.Locale]string{
+//		discordgo.ChineseCN: "这是一个本地化的命令",
+//	},
+//	Options: []*discordgo.ApplicationCommandOption{
+//		{
+//			Name:        "localized-option",
+//			Description: "Localized option. Description and name may vary depending on the Language setting",
+//			NameLocalizations: map[discordgo.Locale]string{
+//				discordgo.ChineseCN: "一个本地化的选项",
+//			},
+//			DescriptionLocalizations: map[discordgo.Locale]string{
+//				discordgo.ChineseCN: "这是一个本地化的选项",
+//			},
+//			Type: discordgo.ApplicationCommandOptionInteger,
+//			Choices: []*discordgo.ApplicationCommandOptionChoice{
+//				{
+//					Name: "First",
+//					NameLocalizations: map[discordgo.Locale]string{
+//						discordgo.ChineseCN: "一的",
+//					},
+//					Value: 1,
+//				},
+//				{
+//					Name: "Second",
+//					NameLocalizations: map[discordgo.Locale]string{
+//						discordgo.ChineseCN: "二的",
+//					},
+//					Value: 2,
+//				},
+//			},
+//		},
+//	},
+//},
+
+//"subcommands": {
+//	Name:        "subcommands",
+//	Description: "Subcommands and command groups example",
+//	Options: []*discordgo.ApplicationCommandOption{
+//		// When a command has subcommands/subcommand groups
+//		// It must not have top-level options, they aren't accesible in the UI
+//		// in this case (at least not yet), so if a command has
+//		// subcommands/subcommand any groups registering top-level options
+//		// will cause the registration of the command to fail
+//
+//		{
+//			Name:        "subcommand-group",
+//			Description: "Subcommands group",
+//			Options: []*discordgo.ApplicationCommandOption{
+//				// Also, subcommand groups aren't capable of
+//				// containing options, by the name of them, you can see
+//				// they can only contain subcommands
+//				{
+//					Name:        "nested-subcommand",
+//					Description: "Nested subcommand",
+//					Type:        discordgo.ApplicationCommandOptionSubCommand,
+//				},
+//			},
+//			Type: discordgo.ApplicationCommandOptionSubCommandGroup,
+//		},
+//		// Also, you can create both subcommand groups and subcommands
+//		// in the command at the same time. But, there's some limits to
+//		// nesting, count of subcommands (top level and nested) and options.
+//		// Read the intro of slash-commands docs on Discord dev portal
+//		// to get more information
+//		{
+//			Name:        "subcommand",
+//			Description: "Top-level subcommand",
+//			Type:        discordgo.ApplicationCommandOptionSubCommand,
+//		},
+//	},
+//},
+
+//}
