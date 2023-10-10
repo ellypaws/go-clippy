85fbfb956a23d85d11f031cc72fd92f5689c498a
Elly <dev@keiau.space>
2023-10-10 22:34:06 +0800

    Update clippy collection to include the username snowflake and points to have the guild info.
    Now we're accepting a `[]Points` as we might have different points from multiple guilds.

--- database/clippy/clippy.go
+++ database/clippy/clippy.go
@@ -1,16 +1,26 @@
 package clippy
 
-import "github.com/nokusukun/bingo"
+import (
+	"github.com/nokusukun/bingo"
+	"strconv"
+)
 
 var ClippyCollection *bingo.Collection[Clippy]
 
 type Clippy struct {
-	Username string `json:"username,omitempty" validate:"required,email"`
-	Points   int    `json:"points,omitempty" validate:"required,min=3,max=64"`
+	Username  string   `json:"username,omitempty"`
+	Snowflake int      `json:"snowflake,omitempty"`
+	Points    []Points `json:"points,omitempty"`
+}
+
+type Points struct {
+	Score   int    `json:"score,omitempty"`
+	Guild   string `json:"guild,omitempty"`
+	GuildID int    `json:"guild_id,omitempty"`
 }
 
 func (c Clippy) Key() []byte {
-	return []byte(c.Username)
+	return []byte(strconv.Itoa(c.Snowflake))
 }
 
 func (c Clippy) Record() {
