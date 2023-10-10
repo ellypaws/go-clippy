4555cf1829e219fedd942422b3b43feaaffbd7e4
Elly <dev@keiau.space>
2023-10-10 21:16:45 +0800

    Added token sanitize in error messages
    Now there's an ephemeral response for errors, and it's implemented in the deletion button if it doesn't have permission to delete in that channel, for example.

--- bot/components.go
+++ bot/components.go
@@ -3,6 +3,8 @@ package discord
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"log"
+	"strings"
 )
 
 const (
@@ -45,13 +47,13 @@ var componentHandlers = map[string]func(bot *discordgo.Session, i *discordgo.Int
 	deleteButton: func(bot *discordgo.Session, i *discordgo.InteractionCreate) {
 		err := bot.ChannelMessageDelete(i.ChannelID, i.Message.ID)
 		if err != nil {
-			return
+			errorEphemeral(bot, i.Interaction, err)
 		}
 	},
 }
 
-// ErrorFollowup sends an error message as a followup message with a deletion button.
-func ErrorFollowup(bot *discordgo.Session, i *discordgo.Interaction, errorContent ...any) {
+// errorFollowup [ErrorFollowup] sends an error message as a followup message with a deletion button.
+func errorFollowup(bot *discordgo.Session, i *discordgo.Interaction, errorContent ...any) {
 	if errorContent == nil || len(errorContent) == 0 {
 		return
 	}
@@ -69,13 +71,13 @@ func ErrorFollowup(bot *discordgo.Session, i *discordgo.Interaction, errorConten
 	components := []discordgo.MessageComponent{components[deleteButton]}
 
 	_, _ = bot.FollowupMessageCreate(i, true, &discordgo.WebhookParams{
-		Content:    errorString,
+		Content:    *sanitizeToken(&errorString),
 		Components: components,
 	})
 }
 
-// ErrorHandler responds to the interaction with an error message and a deletion button.
-func ErrorHandler(bot *discordgo.Session, i *discordgo.Interaction, errorContent ...any) {
+// errorEdit [ErrorResponse] responds to the interaction with an error message and a deletion button.
+func errorEdit(bot *discordgo.Session, i *discordgo.Interaction, errorContent ...any) {
 	if errorContent == nil || len(errorContent) == 0 {
 		return
 	}
@@ -90,10 +92,50 @@ func ErrorHandler(bot *discordgo.Session, i *discordgo.Interaction, errorContent
 		errorString = "An unknown error has occurred"
 		errorString += "\nReceived:" + fmt.Sprint(content)
 	}
+
 	components := []discordgo.MessageComponent{components[deleteButton]}
 
 	_, _ = bot.InteractionResponseEdit(i, &discordgo.WebhookEdit{
-		Content:    &errorString,
+		Content:    sanitizeToken(&errorString),
 		Components: &components,
 	})
+}
+
+// errorEphemeral [ErrorEphemeral] responds to the interaction with an ephemeral error message when the deletion button doesn't work.
+func errorEphemeral(bot *discordgo.Session, i *discordgo.Interaction, errorContent ...any) {
+	if errorContent == nil || len(errorContent) == 0 {
+		return
+	}
+	var errorString string
+
+	switch content := errorContent[0].(type) {
+	case string:
+		errorString = content
+	case error:
+		errorString = fmt.Sprint(content) // Convert the error to a string
+	default:
+		errorString = "An unknown error has occurred"
+		errorString += "\nReceived:" + fmt.Sprint(content)
+	}
+
+	_ = bot.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			// Note: this isn't documented, but you can use that if you want to.
+			// This flag just allows you to create messages visible only for the caller of the command
+			// (user who triggered the command)
+			Flags:   discordgo.MessageFlagsEphemeral,
+			Content: "Could not run the button, got an error: " + *sanitizeToken(&errorString),
+		},
+	})
+}
+
+func sanitizeToken(errorString *string) *string {
+	if strings.Contains(*errorString, *botToken) {
+		log.Println("WARNING: Bot token was found in the error message. Replacing it with \"Bot Token\"")
+		log.Println("Error message:", errorString)
+		sanitizedString := strings.ReplaceAll(*errorString, *botToken, "[TOKEN]")
+		errorString = &sanitizedString
+	}
+	return errorString
 }
--- bot/handlers.go
+++ bot/handlers.go
@@ -12,8 +12,8 @@ var commandHandlers = map[string]func(bot *discordgo.Session, i *discordgo.Inter
 	},
 	solvedCommand: func(bot *discordgo.Session, i *discordgo.InteractionCreate) {
 		responses[pendingResponse].(regularResponseType)(bot, i)
-		responses[errorResponse].(errorResponseType)(bot, i.Interaction, "This command is not implemented yet")
-		responses[errorFollowup].(errorResponseType)(bot, i.Interaction, "Testing followup error message")
+		responses[ErrorResponse].(errorResponseType)(bot, i.Interaction, "This command is not implemented yet")
+		responses[ErrorFollowup].(errorResponseType)(bot, i.Interaction, "Testing followup error message")
 	},
 	functionCommand: func(bot *discordgo.Session, i *discordgo.InteractionCreate) {
 
@@ -27,8 +27,9 @@ const (
 	pendingResponse
 	ephemeralResponse
 	helloResponse
-	errorResponse
-	errorFollowup
+	ErrorResponse
+	ErrorFollowup
+	ErrorEphemeral
 )
 
 type regularResponseType func(bot *discordgo.Session, i *discordgo.InteractionCreate)
@@ -41,7 +42,9 @@ var responses = map[int]any{
 		err := bot.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseType(i.ApplicationCommandData().Options[0].IntValue()),
 		})
-		ErrorHandler(bot, i.Interaction, err)
+		if err != nil {
+			errorEdit(bot, i.Interaction, err)
+		}
 	},
 	pendingResponse: regularResponseType(func(bot *discordgo.Session, i *discordgo.InteractionCreate) {
 		err := bot.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
@@ -54,7 +57,9 @@ var responses = map[int]any{
 				Content: "Bot is responding...",
 			},
 		})
-		ErrorHandler(bot, i.Interaction, err)
+		if err != nil {
+			errorEdit(bot, i.Interaction, err)
+		}
 	}),
 	ephemeralResponse: regularResponseType(func(bot *discordgo.Session, i *discordgo.InteractionCreate) {
 		err := bot.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
@@ -67,7 +72,9 @@ var responses = map[int]any{
 				Content: "Bot is responding...",
 			},
 		})
-		ErrorHandler(bot, i.Interaction, err)
+		if err != nil {
+			errorEdit(bot, i.Interaction, err)
+		}
 	}),
 	helloResponse: regularResponseType(func(bot *discordgo.Session, i *discordgo.InteractionCreate) {
 		err := bot.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
@@ -76,10 +83,13 @@ var responses = map[int]any{
 				Content: "Hey there! Congratulations, you just executed your first slash command",
 			},
 		})
-		ErrorHandler(bot, i.Interaction, err)
+		if err != nil {
+			errorEdit(bot, i.Interaction, err)
+		}
 	}),
-	errorResponse: errorResponseType(ErrorHandler),
-	errorFollowup: errorResponseType(ErrorFollowup),
+	ErrorResponse:  errorResponseType(errorEdit),
+	ErrorFollowup:  errorResponseType(errorFollowup),
+	ErrorEphemeral: errorResponseType(errorEphemeral),
 }
 
 // ----- UNUSED COMMAND HANDLERS -----
