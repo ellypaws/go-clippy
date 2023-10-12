bb867b4dd08a2b1bc13ff31cb16beb9b81a3aa9d
Elly <dev@keiau.space>
2023-10-12 13:41:19 +0800

    Implement basic CRUD for clippy\query.go

--- database/clippy/query.go
+++ database/clippy/query.go
@@ -12,3 +12,29 @@ func (c Clippy) Record() {
 	}
 	fmt.Println("Inserted", id)
 }
+
+func RecordMany(c []Clippy) {
+	id, err := Collection.InsertMany(c, bingo.Upsert)
+	if err != nil {
+		panic(err)
+	}
+	for _, i := range id {
+		fmt.Println("Inserted", i)
+	}
+}
+
+func GetClippy(s string) (*Clippy, error) {
+	q := QueryClippy(s)
+	if q.Error != nil {
+		return nil, q.Error
+	}
+	return q.First(), nil
+}
+
+func QueryClippy(s string) *bingo.QueryResult[Clippy] {
+	return Collection.Query(bingo.Query[Clippy]{
+		Filter: func(doc Clippy) bool {
+			return doc.Snowflake == s
+		},
+	})
+}
