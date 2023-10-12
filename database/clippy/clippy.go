c92f882a6e855c31ef8f5be306314b4234cc9e48
Elly <dev@keiau.space>
2023-10-12 12:02:38 +0800

    Change snowflake to string as this is what discordgo gives us

--- database/clippy/clippy.go
+++ database/clippy/clippy.go
@@ -1,15 +1,12 @@
 package clippy
 
-import (
-	"github.com/nokusukun/bingo"
-	"strconv"
-)
+import "github.com/nokusukun/bingo"
 
 var ClippyCollection *bingo.Collection[Clippy]
 
 type Clippy struct {
 	Username  string   `json:"username,omitempty"`
-	Snowflake int      `json:"snowflake,omitempty"`
+	Snowflake string   `json:"snowflake,omitempty"`
 	Points    []Points `json:"points,omitempty"`
 }
 
@@ -20,7 +17,7 @@ type Points struct {
 }
 
 func (c Clippy) Key() []byte {
-	return []byte(strconv.Itoa(c.Snowflake))
+	return []byte(c.Snowflake)
 }
 
 func (c Clippy) Record() {
